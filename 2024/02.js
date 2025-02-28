339e81f307da6aa39f3d7f15636324bc750c5327
wliu080 <simonliu911@gmail.com>
2025-02-28 10:29:28 +0900

    day 02 part 1 completed

--- 2024/02.js
+++ 2024/02.js
@@ -8,45 +8,63 @@ const input = fs.readFileSync(inputFile, "utf-8")
  *  - Each report will always have at least 1 level
  *
  * Notes for self / reflection
- *  - Part one took (so far: reading problem + approach one (using .every) 20min, .reduce approach 15min)
+ *  - Part one took ~55min (reading problem + approach one (using .every) 20min, .reduce approach 15min, inc/dec check functionality 15min, parsing input 5min)
  *    - On first glance, wanted to just do a reduce, but we want to break early if it changes from increasing to decreasing or vice versa
  *    - Spent some time doing it as a .every and just breaking out early but reduce is so much of what we want and I think ultimately it doesn't really matter since each individual report is never too long
  *  - Part two took
  */
 
-//const reports = partOne(input.trim())
-const reports = isSafe([1, 2, 7, 8, 9])
+const reports = partOne(input.trim())
 
-// Calculate diff for each
 console.log("#safe reports")
-console.log(reports)
+console.log(reports.filter(isSafe).length)
 console.log("============")
 
-function partOne(input) {}
+// parses string input and returns a list of reports (list of numbers)
+function partOne(input) {
+    const lines = input.split("\n")
+    return lines.map((line) => {
+        const reportLine = line.split(" ")
+        return reportLine.map((level) => {
+            return Number(level)
+        })
+    })
+}
 
+/**
+ * Checks a report for safety based on the following:
+ * - levels must all be either increasing or decreasing
+ * - the difference between adjacent levels can't be less than 1 (i.e, the same number) or more than 3
+ *
+ * @param {Array<number>} report
+ * @returns {boolean} True if the report is considered safe
+ */
 function isSafe(report) {
-    // must be all be increasing or decreasing (can't stay the same level)
-    // adjacent levels can't be too different (1-3)
-
     let isPrevIncreasing
     return report.reduce((prevLevel, nextLevel, idx) => {
-        console.log("checking prev[%d] next[%d] idx[%d]", prevLevel, nextLevel, idx)
-
         // if we have failed already, pass through
         if (prevLevel === false) {
             return false
         }
 
-        // check if we change directions from increasing to decreasing or vice versa
+        const diff = prevLevel - nextLevel
 
         // check adjacent level differences
-        let diff = Math.abs(prevLevel - nextLevel)
-        if (diff < 1 || diff > 3) {
+        const absoluteDiff = Math.abs(diff)
+        if (absoluteDiff < 1 || absoluteDiff > 3) {
             return false
         }
 
+        // check if we change directions from increasing to decreasing or vice versa
+        const isCurrentlyIncreasing = diff < 0
+        if (isPrevIncreasing != null && isPrevIncreasing != isCurrentlyIncreasing) {
+            return false
+        }
+        isPrevIncreasing = isCurrentlyIncreasing
+
         // if we are at the end and no fails, report is safe
         if (idx === report.length - 1) {
+            console.log("report %s is safe", report)
             return true
         } else {
             return nextLevel
