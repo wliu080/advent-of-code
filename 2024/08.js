16f68e5db5abe28901e8474d84aceeb6105fa813
wliu080 <simonliu911@gmail.com>
2025-03-27 01:17:02 +1300

    day 8 complete

--- 2024/08.js
+++ 2024/08.js
@@ -11,14 +11,16 @@ const input = fs.readFileSync(inputFile, "utf-8")
  *  - Part one 20min tests setup, 1hr25min antinode logic, 20min filtering / parsing result
  *      - initial thoughts:
  *          - maybe just read the input as a map of coordinates, e.g, 'a': [{0,2}, {1,4}, ...] and then find the translations between every coord
- *  - Part two
+ *  - Part two 28min solve
+ *      - there's quite a bit of code duplication, but I think the rewrite to make it reusable for both parts would've been not worth the effort for something like this
+ *      - maybe there's merit to rewriting it such that the one function would return both the regular antinodes as well as the harmonic version?
  */
 
 console.log("=== pt1 ===")
 console.log(partOne(input.trim()))
 console.log("===========================")
 console.log("=== pt2 ===")
-//console.log(partTwo(input.trim()))
+console.log(partTwo(input.trim()))
 console.log("========================")
 
 function partOne(input) {
@@ -32,6 +34,18 @@ function partOne(input) {
 
     // remove any that are out of bounds
     masterAntinodes = constrainBy(metadata.rowBounds, metadata.colBounds, masterAntinodes)
+
+    return _.uniqWith(masterAntinodes, _.isEqual).length
+}
+
+function partTwo(input) {
+    const [map, metadata] = toMap(input)
+    let masterAntinodes = []
+
+    // calculate antinodes
+    metadata.keys.forEach((key) => {
+        masterAntinodes.push(...deriveBoundedHarmonicAntinodes(map[key], metadata.rowBounds, metadata.colBounds))
+    })
 
     return _.uniqWith(masterAntinodes, _.isEqual).length
 }
@@ -67,6 +81,47 @@ function deriveAntinodes(coords) {
     return _.uniqWith(antinodes, _.isEqual)
 }
 
+function deriveBoundedHarmonicAntinodes(coords, rowBounds, colBounds) {
+    let antinodes = []
+    for (let i = 0; i < coords.length; i++) {
+        const aRow = coords[i].row
+        const aCol = coords[i].col
+        for (let j = i + 1; j < coords.length; j++) {
+            const bRow = coords[j].row
+            const bCol = coords[j].col
+            const rowDiff = bRow - aRow
+            const colDiff = bCol - aCol
+
+            // as long as there are two nodes of the same freq, they themselves are antinodes
+            antinodes.push({ row: aRow, col: aCol })
+
+            for (
+                let nextRow = aRow - rowDiff, nextCol = aCol - colDiff;
+                nextRow >= 0 && nextRow < rowBounds && nextCol >= 0 && nextCol < colBounds;
+                nextRow -= rowDiff, nextCol -= colDiff
+            ) {
+                antinodes.push({
+                    row: nextRow,
+                    col: nextCol,
+                })
+            }
+
+            for (
+                let nextRow = aRow + rowDiff, nextCol = aCol + colDiff;
+                nextRow >= 0 && nextRow < rowBounds && nextCol >= 0 && nextCol < colBounds;
+                nextRow += rowDiff, nextCol += colDiff
+            ) {
+                antinodes.push({
+                    row: nextRow,
+                    col: nextCol,
+                })
+            }
+        }
+    }
+    //remove duplicates (when multiple nodes have overlapping antinodes) before returning
+    return _.uniqWith(antinodes, _.isEqual)
+}
+
 function toMap(input) {
     const regex = /^[a-zA-Z0-9]$/g
     let map = []
@@ -89,7 +144,8 @@ function toMap(input) {
 }
 
 // Part one test cases
-const TEST_FOLDER = "2024/tests/08/"
+const PT1_TEST_FOLDER = "2024/tests/08_1/"
+const PT2_TEST_FOLDER = "2024/tests/08_2/"
 
 console.log("--antinode tests--")
 const antinodeTestCases = [
@@ -201,15 +257,15 @@ antinodeTestCases.forEach(({ input, expected }, idx) => {
     console.log(`Test case ${idx + 1}:`, _.isEqual(result, sortedExpected) ? "Passed" : "Failed")
 })
 
-console.log("--pt1 tests--")
-fs.readdir(TEST_FOLDER, (err, files) => {
+fs.readdir(PT1_TEST_FOLDER, (err, files) => {
+    console.log("--pt1 tests--")
     if (err) {
         console.error("could not read test files")
         return
     }
 
     files.forEach((filename, idx) => {
-        const file = fs.readFileSync(TEST_FOLDER + filename, "utf-8")
+        const file = fs.readFileSync(PT1_TEST_FOLDER + filename, "utf-8")
         let expected = Number(file.split("!\n")[0])
         let input = file.split("!\n")[1]
 
@@ -223,27 +279,24 @@ fs.readdir(TEST_FOLDER, (err, files) => {
     })
 })
 
-// console.log("--pt 2 tests--")
-// const partTwoTests = [
-//     {
-//         input: `190: 10 19
-// 3267: 81 40 27
-// 83: 17 5
-// 156: 15 6
-// 7290: 6 8 6 15
-// 161011: 16 10 13
-// 192: 17 8 14
-// 21037: 9 7 18 13
-// 292: 11 6 16 20`,
-//         expected: 11387,
-//     },
-// ]
-// partTwoTests.forEach(({ input, expected }, index) => {
-//     const result = partTwo(input)
+fs.readdir(PT2_TEST_FOLDER, (err, files) => {
+    console.log("--pt2 tests--")
+    if (err) {
+        console.error("could not read test files")
+        return
+    }
 
-//     console.assert(
-//         result === expected,
-//         `Test case ${index + 1} failed: Input ${input}. Expected ${expected}, got ${result}`
-//     )
-//     console.log(`Test case ${index + 1}:`, result === expected ? "Passed" : "Failed")
-// })
+    files.forEach((filename, idx) => {
+        const file = fs.readFileSync(PT2_TEST_FOLDER + filename, "utf-8")
+        let expected = Number(file.split("!\n")[0])
+        let input = file.split("!\n")[1]
+
+        const result = partTwo(input)
+
+        console.assert(
+            result === expected,
+            `Test case ${idx + 1} failed: Input ${input}. Expected ${expected}, got ${result}`
+        )
+        console.log(`Test case ${idx + 1}:`, result === expected ? "Passed" : "Failed")
+    })
+})
